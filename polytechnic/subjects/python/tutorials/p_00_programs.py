8bfe95d7faa264a3e0a5cee969537933f5fa90b6
parvezk <parvezk>
2023-07-16 18:57:27 +0530

    python program practical

---
+++ polytechnic/subjects/python/tutorials/p_00_programs.py
@@ -0,0 +1,19 @@
+# WAP to add, multiply two input numbers
+
+# WAP to print remainder of division
+
+# WAP to print largest of 2, 3, 4, 5 numbers
+
+# WAP to print number is even-odd
+
+# WAP to print table
+
+# Write functions to add, divide and multiply two numbers
+
+# WAP to implement student class
+
+# WAP to implement Result class
+
+# WAP to implement Stack class
+
+# WAP to implement file operations
