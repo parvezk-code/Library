168d1c659bf7d8cc2d9945feb91fbc52744a1d69
parvezk <parvezk>
2023-08-24 00:42:23 +0530

    latex help

---
+++ Learning/programming/Latex/help/code/coding_01.c
@@ -0,0 +1,35 @@
+\usepackage{listings}
+
+\begin{lstlisting}
+    # coding goes here
+\end{lstlisting}
+
+# \lstinputlisting[language=Octave]{hello.java}
+
+\lstset{style=mystyle}
+
+\lstdefinestyle{mystyle}{
+    backgroundcolor=\color{backcolour},
+    commentstyle=\color{codegreen},
+    keywordstyle=\color{magenta},
+    numberstyle=\tiny\color{codegray},
+    stringstyle=\color{codepurple},
+    basicstyle=\ttfamily\footnotesize,
+    breakatwhitespace=false,
+    breaklines=true,
+    captionpos=b,
+    keepspaces=true,
+    numbers=left,
+    numbersep=5pt,
+    showspaces=false,
+    showstringspaces=false,
+    showtabs=false,
+    tabsize=2
+}
+
+\usepackage{xcolor}
+
+\definecolor{codegreen}{rgb}{0,0.6,0}
+\definecolor{codegray}{rgb}{0.5,0.5,0.5}
+\definecolor{codepurple}{rgb}{0.58,0,0.82}
+\definecolor{backcolour}{rgb}{0.95,0.95,0.92}
