8e2667b0d4bada668c9c4f1b86995d8225d30fd1
parvezk <parvezk>
2023-08-24 09:24:14 +0530

    latex help

---
+++ Learning/programming/Latex/help/code/exam_questions_01.c
@@ -0,0 +1,20 @@
+\documentclass{exam}
+
+\begin{questions}
+
+    \printanswers
+
+    \question #My first question with multiple choices.
+
+        \begin{choices}
+            \choice           The first choice.
+            \correctchoice    The second choice.
+            \choice           The third choice.
+            \choice           The fourth choice.
+        \end{choices}
+
+\end{questions}
+
+# \begin{checkboxes}   \end{checkboxes}
+# fill in the blank : \fillin[Helium]
+# True False :  \tf[T]
