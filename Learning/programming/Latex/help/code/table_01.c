06f92031c107a1a033697b80d292ad590852fb58
parvezk <parvezk>
2023-08-24 09:11:22 +0530

    latex help

--- Learning/programming/Latex/help/code/table_01.c
+++ Learning/programming/Latex/help/code/table_01.c
@@ -3,6 +3,10 @@
         City        &    Bhopal     \\ \hline
         Subject     &    Maths     \\ \hline
         Fruit       &    Mango     \\ \hline
+        \multicolumn{2}{|c|}{ Hello World} \\ \hline
     \end{tabular}
 \end{myTableStyle}
 \vspace{0.08in}
+
+# \multicolumn{numcols}{format}{text}
+# \multirow[vert-pos]{num-rows}{width}[vert-adjust]{text}
