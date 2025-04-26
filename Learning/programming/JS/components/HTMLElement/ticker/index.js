67121e29391303821d553c1186214177e0f6be3c
parvezk <parvezk>
2025-04-26 15:54:39 +0530

    re structured components

--- Learning/programming/JS/components/HTMLElement/ticker/index.js
+++ Learning/programming/JS/components/HTMLElement/ticker/index.js
@@ -4,46 +4,54 @@ class Controller
     {
         this.root = document.getElementById(rootId);
         if (!this.root) throw new Error(`Element with ID '${rootId}' not found.`);
-        this.mainContent = document.createElement('div');
-        this.mainContent.className = 'main-content';
     }
   
     init() 
     {
-        this.root.appendChild(this.mainContent);
         this.renderTickers_01();
+        this.renderVerticalSpace('5vh');
         this.renderTickers_04();
+        this.renderVerticalSpace('5vh');
         this.renderTickers_03();
+        this.renderVerticalSpace('5vh');
         this.renderTickers_05();
+        this.renderVerticalSpace('5vh');
         this.renderCarousel_06();
+    }
+    
+    renderVerticalSpace(size)
+    {
+        const space = new VerticalSpace();
+        space.setAttribute('height', `${size}`);
+        this.root.appendChild(space);
     }
   
     renderTickers_01()
     {
         const ticker = document.createElement('content-ticker-01');
         ticker.setAttribute('config', JSON.stringify(window.siteData?.tickerConfig?.config01 || {}));
-        this.mainContent.appendChild(ticker);
+        this.root.appendChild(ticker);
     }
 
     renderTickers_03() {
         const scroller = document.createElement('image-scroller-03');
         const config = window.siteData?.tickerConfig?.config03 || {};
         scroller.setAttribute('config', JSON.stringify(config));
-        this.mainContent.appendChild(scroller);
+        this.root.appendChild(scroller);
     }
     
     renderTickers_04() {
         const scroller = document.createElement('card-scroller-04');
         const config = window.siteData?.tickerConfig?.config02 || {};
         scroller.setAttribute('config', JSON.stringify(config));
-        this.mainContent.appendChild(scroller);
+        this.root.appendChild(scroller);
     }
 
     renderTickers_05() {
         const scroller = document.createElement('image-carousel05');
         const config = window.siteData?.tickerConfig?.config03 || {};
         scroller.setAttribute('config', JSON.stringify(config));
-        this.mainContent.appendChild(scroller);
+        this.root.appendChild(scroller);
         // new FlickityCarousel04(scroller);
     }
 
@@ -59,7 +67,7 @@ class Controller
           scroller.appendChild(img);
         });
       
-        this.mainContent.appendChild(scroller);
+        this.root.appendChild(scroller);
       }
 }
 
